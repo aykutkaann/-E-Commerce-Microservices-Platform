f983bc20a609f04f12d50e61530633d90e00d5c8
Aykut <aykut00kaan@outlook.com>
2026-03-10 19:00:54 +0300

    feat: kubernetes added

---
+++ src/UserService/Migrations/20260310155231_FixPendingChanges.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UserService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { -2, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "jhon@example.com", "Jhon", "Dev", "5559876543" },
+                    { -1, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "aykut@example.com", "Aykut Kaan", "Altundal", "5551234567" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: -2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: -1);
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedAt", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "aykut@example.com", "Aykut Kaan", "Altundal", "5551234567" },
+                    { 2, new DateTime(2026, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "jhon@example.com", "Jhon", "Dev", "5559876543" }
+                });
+        }
+    }
+}
